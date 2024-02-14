efb5367b18193fabc275baabf3d1944f53822cf5
Vigolajnen Anna <vigolajnen@gmail.com>
2024-02-14 19:33:32 +0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import Layout from '../components/layout/layout';
+
+export default function AboutPage() {
+  return (
+    <Layout pageTitle='About page'>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+}
+
+// You'll learn about this in the next task, just copy it for now
+export const Head = () => (<>
+<title>About Me</title>
+<meta name="description" content="123123 Your description" />
+</>);
